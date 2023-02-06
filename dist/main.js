9736b9966d85a2fa51efc1b7bc3a44524d59a5b2
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-02-06 20:53:09 +0100

    setup project with webpack and jest

---
+++ dist/main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("// const fetchUser = async (url) => {\n//   const resp = await fetch(url);\n//   const data = await resp.json();\n//   console.log(data[0]['Image Gallery'][0].src);\n//   const name = data[0]['Species Name'];\n\n//   const resp2 = await fetch(`https://www.fishwatch.gov/api/species/${name}`);\n//   const body = await resp2.json();\n//   console.log(body);\n// };\n\n// fetchUser('https://www.fishwatch.gov/api/species');\n\n\n//# sourceURL=webpack://capstone-module2/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
