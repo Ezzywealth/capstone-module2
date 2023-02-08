6efc4d23e78199ef8df5bc47ee335e220f0b42bc
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-02-08 21:32:09 +0100

    deleted node modules

--- modules/commentCount.js
+++ modules/commentCount.js
@@ -1,6 +1,5 @@
 const commentCount = (section) => {
   const commentsContainer = document.getElementById('comment-title');
-
   const items = Array.from(section.children);
   commentsContainer.innerText += ` (${items.length})`;
   return items;
