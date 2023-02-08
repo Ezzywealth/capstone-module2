bfabc9eb90423ec2371c0e19a89ae67980ff44cd
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-02-09 00:08:44 +0100

    fixed linters errors

--- modules/displayComments.js
+++ modules/displayComments.js
@@ -1,8 +1,17 @@
 const displayComments = (comments, commentsContainer) => {
-  comments?.forEach((comment) => {
+  comments?.forEach((item) => {
+    const { creation_date: creationDate, username, comment } = item;
     const newComments = document.createElement('div');
+    const dateSpan = document.createElement('span');
+    dateSpan.innerText = creationDate;
+    const nameSpan = document.createElement('span');
+    nameSpan.innerText = username;
+    const commentSpan = document.createElement('span');
+    commentSpan.innerText = comment;
     newComments.className = 'comments-list';
-    newComments.innerText = ` ${comment.creation_date} ${comment.username} : ${comment.comment} `;
+    newComments.appendChild(dateSpan);
+    newComments.appendChild(nameSpan);
+    newComments.appendChild(commentSpan);
     commentsContainer.appendChild(newComments);
   });
 };
