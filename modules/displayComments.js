3516fd131a4c1103e1c836d3f355ba1e5d8641a0
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-02-09 08:30:18 +0100

    fix eslint errors

--- modules/displayComments.js
+++ modules/displayComments.js
@@ -2,19 +2,7 @@ const displayComments = (comments, commentsContainer) => {
   comments?.forEach((item) => {
     const { creation_date: creationDate, username, comment } = item;
     const newComments = document.createElement('div');
-    const dateSpan = document.createElement('span');
-    dateSpan.innerText = creationDate;
-    const nameSpan = document.createElement('span');
-    nameSpan.innerText = username;
-    const commentSpan = document.createElement('span');
-    const dividerSpan = document.createElement('span');
-    dividerSpan.innerText = ':';
-    commentSpan.innerText = comment;
-    newComments.className = 'comments-list';
-    newComments.appendChild(dateSpan);
-    newComments.appendChild(nameSpan);
-    newComments.appendChild(dividerSpan);
-    newComments.appendChild(commentSpan);
+    newComments.innerText = `${creationDate} ${username} : ${comment}`;
     commentsContainer.appendChild(newComments);
   });
 };
