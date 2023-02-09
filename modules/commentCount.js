8798241100fb6a2842f91154e3bcd8400b8cd400
aubin sinpeze <aubinsimpeze01@gmail.com>
2023-02-09 13:59:22 +0200

    Add a test for the commentCount function

--- modules/commentCount.js
+++ modules/commentCount.js
@@ -5,3 +5,4 @@ const commentCount = (section) => {
   return items;
 };
 export default commentCount;
+
---
+++ modules/tests/commentCount.test.js
@@ -0,0 +1,51 @@
+import commentCount from "../commentCount.js";
+import displayComments from "../displayComments.js";
+
+const comments = [
+  {
+    creation_date:'2023-02-08',
+    username:'aubin',
+    comment:'Nice one',
+  },
+  {
+    creation_date:'2023-02-08',
+    username:'John',
+    comment:'Good',
+  },
+  {
+    creation_date:'2023-02-08',
+    username:'Marc',
+    comment:'Great',
+  },
+ 
+];
+describe('TodoApp', () => {
+  test('Number of comments In the DOM before API fetch', () => {
+
+   document.body.innerHTML = `
+   <div class="comment-container">
+   <h3 id="comment-title">Comments </h3>
+   <div id="comment-list">
+
+   </div>
+  </div`;
+    const section = document.getElementById('comment-list');
+    expect(commentCount(section)).toHaveLength(0);
+  });
+
+  test('Number of comments displayed in the DOM after a successful API call', () => {
+    document.body.innerHTML = `
+ 
+    <div class="comment-container">
+    <h3 id="comment-title">Comments </h3>
+    <div id="comment-list">
+
+    </div>
+   </div>`;
+   const section = document.getElementById('comment-list');
+    const commentsContainer = document.querySelector('.comment-container');
+    displayComments(comments, section)
+    expect(commentCount(section)).toHaveLength(3);
+  });
+});
+
